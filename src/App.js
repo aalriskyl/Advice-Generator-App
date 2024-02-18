e61f9847cd3cf77fd7586e81e41b4a366967f4ce
aalriskyl <aalriskyl3105@gmail.com>
2024-02-18 17:25:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Divider from './images/pattern-divider-desktop.svg';
+import IconDice from './images/icon-dice.svg';
+
 
 function App() {
+  const [adviceData, setAdviceData] = useState('');
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      if (!response.ok) {
+        throw new Error('Failed to fetch advice');
+      }
+      const data = await response.json();
+      setAdviceData(data.slip);
+    } catch (error) {
+      console.error('Error fetching advice:', error);
+    }
+  };
+  const handleGenerateAdvice = () => {
+    fetchAdvice();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+       <div className="container-box">
+        {adviceData && (
+          <div>
+            <h1 className="container-title">Advice #{adviceData.id}</h1>
+            <p>"{adviceData.advice}"</p>
+            <img className="img-divider" src={Divider} alt="Divider" />
+          </div>
+        )}
+        <img className="generate-button" src={IconDice} alt="Generate Advice" onClick={handleGenerateAdvice} />
+      </div>
     </div>
   );
 }
